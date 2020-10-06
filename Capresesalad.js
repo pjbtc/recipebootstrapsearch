dbbad4a4d5a1253532054de5b021ab8eab1b6693
pjbtc <pjbhattachan@gmail.com>
2020-10-06 13:47:33 +0100

    Add files via upload

---
+++ Capresesalad.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Capresesalad=()=>{
+	return(
+		<div className="newcontainer">
+			<div className= "col justify-content-md-center my-5">
+		<h1> CapreseSalad recipe</h1>
+		<p> Ingredient</p>
+		<ul>
+			<li>Heirloom Tomatoes </li>
+			<li>Buffalo mozerella</li>
+			<li>Black pepper</li>
+		</ul>
+		
+		</div>
+		</div>
+		)
+}
+export default Capresesalad;
