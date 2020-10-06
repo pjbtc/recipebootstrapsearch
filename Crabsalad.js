c07a4fbf706a773d5687f06a5c8c26c187016770
pjbtc <pjbhattachan@gmail.com>
2020-10-06 13:48:43 +0100

    Add files via upload

---
+++ Crabsalad.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Crabsalad=()=>{
+	return(
+		<div className="newcontainer">
+			<div className= "row justify-content-md-center my-5">
+		<h1> Crab meat recipe</h1>
+		<ul>
+			<li>Mayonaise </li>
+			<li>icing sugar</li>
+			<li>Lemon and orange juice </li>
+		</ul>
+		<h1> Ingredient</h1>
+		</div>
+		</div>
+		)
+}
+export default Crabsalad;
