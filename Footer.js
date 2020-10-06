6844ac08a80a2a7248b8e809cafd17d40df8c10e
pjbtc <pjbhattachan@gmail.com>
2020-10-06 13:36:33 +0100

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer=()=>{
+	return(
+		<div className="footerpage">
+		
+<nav class="navbar   justify-content-center navbar-light bg-info">
+  <a class="navbar-brand" href="#">TukcheHouse</a>
+</nav>
+
+
+
+ 
+		</div>
+		)
+}
+export default Footer;
