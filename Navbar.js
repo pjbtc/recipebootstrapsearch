2a3fdf563f7273db006f5b6f19914ae4185571b9
pjbtc <pjbhattachan@gmail.com>
2020-10-06 13:32:09 +0100

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,86 @@
+import React,{Component} from 'react';
+
+class Navbar extends Component {
+  state = {
+    searchValue: "",
+    meals: []
+  };
+
+  handleOnChange = event => {
+    this.setState({ searchValue: event.target.value });
+  };
+
+  handleSearch = () => {
+    this.makeApiCall(this.state.searchValue);
+  };
+
+  makeApiCall = searchInput => {
+    var searchUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
+    fetch(searchUrl)
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.setState({ meals: jsonData.meals });
+      });
+  };
+  render(){
+	return(
+	<div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <a class="navbar-brand" href="#">TukcheHouse</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/recipe">Recipe</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Menu
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Dessert</a>
+          <a class="dropdown-item" href="#">Wines</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      
+    </ul>
+   <input
+          name="text"
+          type="text"
+          placeholder="Search"
+          onChange={event => this.handleOnChange(event)}
+          value={this.state.searchValue}
+        />
+        <button onClick={this.handleSearch}>Search</button>
+      
+        {this.state.meals ? (
+          <div id="meals-container">
+            {this.state.meals.map((meal, index) => (
+              <div class="single-meal" key={index}>
+                <h2>{meal.strMeal}</h2>
+                <img src={meal.strMealThumb} alt="meal-thumbnail" />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Try searching for a meal</p>
+        )}
+       
+  </div>
+</nav>
+	</div>
+	)
+
+}
+}
+export default Navbar;
